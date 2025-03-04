2f7147ce983ac5a302b1fdb24b27f9d59cbfb530
unkabas <undercannabis10@gmail.com>
2025-03-04 22:42:03 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import 'react-toastify/dist/ReactToastify.css'
 function App() {
 	return (
 		<>
-			{/* Ваши существующие компоненты */}
 			<ToastContainer
 				position='top-right'
 				autoClose={5000}
